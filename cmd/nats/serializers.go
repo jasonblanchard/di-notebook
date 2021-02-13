414448e4df752c2c7844c08f0cbad44e64f794e0
jasonblanchard <jas.blanchard@gmail.com>
2021-02-13 13:36:07 -0500

    Better serialization

--- cmd/nats/serializers.go
+++ cmd/nats/serializers.go
@@ -12,14 +12,14 @@ import (
 // giving us an anti-corruption layer between ingress consumers, internal entities and egress consumers.
 // In this case, the Entry and Principal entities are flattened for simpler analytics consumption. Arguable whether or not this lives here, but... it does for now.
 type EntryRevision struct {
-	ID         string
-	Text       string
-	CreatorID  string
-	CreatedAt  *time.Time
-	UpdatedAt  *time.Time
-	DeleteTime *time.Time
-	ActorType  string
-	ActorID    string
+	ID         string     `json:"id"`
+	Text       string     `json:"text"`
+	CreatorID  string     `json:"creator_id"`
+	CreatedAt  *time.Time `json:"create_time"`
+	UpdatedAt  *time.Time `json:"update_time"`
+	DeleteTime *time.Time `json:"delete_time"`
+	ActorType  string     `json:"actor_type"`
+	ActorID    string     `json:"actor_id"`
 }
 
 func timestamppbToTimePointer(pbtime *timestamppb.Timestamp) *time.Time {
